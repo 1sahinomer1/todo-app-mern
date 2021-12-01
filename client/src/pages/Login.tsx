e87e1b7d839a01df0b65b5793589442c9ff49fae
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2021-12-01 14:45:35 +0300

    add login pages

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,70 @@
-const Login = () => {
-  return <div>login</div>;
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Link } from 'react-router-dom';
+import * as yup from 'yup';
+
+import { Button, Input } from 'components';
+
+import { formTypes } from 'constants/layouts/loginRegister';
+import LoginRegister from 'layouts/LoginRegister/LoginRegister';
+import { formValidations } from 'utils';
+
+import * as S from 'styles/Pages/Register';
+
+const Register = () => {
+  interface IFormInputs {
+    fullname: string;
+    email: string;
+    password: string;
+    passwordConfirmation: string;
+  }
+  const validationSchema = yup
+    .object()
+    .shape({
+      email: formValidations.emailValidation,
+      password: formValidations.passwordValidation,
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(validationSchema),
+  });
+  const onSubmit = (data: IFormInputs) => console.log(data);
+  return (
+    <LoginRegister formType={formTypes.LOGIN}>
+      <S.Form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          {...register('email')}
+          type="text"
+          label="Email"
+          placeholder="Email"
+          error={errors.email?.message}
+          hasValue={watch('email')}
+        />
+        <Input
+          {...register('password')}
+          type="password"
+          label="Password"
+          placeholder="Password"
+          error={errors.password?.message}
+          hasValue={watch('password')}
+        />
+
+        <Button>Login</Button>
+        <S.Redirect>
+          Don't you have an account ?
+          <Link to="/register">
+            <S.LinkText>Sign Up</S.LinkText>
+          </Link>
+        </S.Redirect>
+      </S.Form>
+    </LoginRegister>
+  );
 };
 
-export default Login;
+export default Register;
