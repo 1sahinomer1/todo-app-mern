c220083cb1d6797821a6a7a41d8e92180efb12b2
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2021-12-01 14:46:04 +0300

    add index export and theme colors and update index css

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as Navbar } from './Navbar/Navbar';
+export { default as Input } from './Input/Input';
+export { default as Button } from './Button/Button';
---
+++ client/src/theme/index.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  successValidate: '#17D7A0',
+  failedValite: '#C85C5C',
+  primaryColor: '#50C2C9',
+};
