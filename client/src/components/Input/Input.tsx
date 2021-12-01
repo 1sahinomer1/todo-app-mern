a55a6051c8b0d263a37182d7eb2f6463edf73b48
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2021-12-01 14:43:37 +0300

    add input comp

---
+++ client/src/components/Input/Input.tsx
@@ -0,0 +1,38 @@
+import { forwardRef, useState } from 'react';
+
+import * as S from './styles';
+
+type InputProps = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  hasValue: string;
+  error: string | undefined;
+  label: string;
+  type: string;
+};
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, hasValue, type, error, ...props }, ref) => {
+    const [showPassword, setShowPassword] = useState(type);
+
+    return (
+      <S.InputWrapper>
+        <S.Label>{label}</S.Label>
+        <S.InputContainer hasValue={hasValue} error={error}>
+          <S.StyledInput type={showPassword} ref={ref} {...props} />
+          {type === 'password' ? (
+            showPassword === 'password' ? (
+              <S.StyledEye onClick={() => setShowPassword('text')} />
+            ) : (
+              <S.StyledHiddenEye onClick={() => setShowPassword('password')} />
+            )
+          ) : null}
+        </S.InputContainer>
+        <S.Error>{error}</S.Error>
+      </S.InputWrapper>
+    );
+  }
+);
+
+export default Input;
---
+++ client/src/components/Input/styles.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import { ForwardedRef } from 'react';
+
+import { Eye, HidedEye } from 'Icons';
+
+import { colors } from 'theme';
+
+interface InputContainerProps {
+  hasValue: string;
+  error: string | undefined;
+}
+interface InputProps {
+  ref: ForwardedRef<HTMLInputElement> | any;
+}
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const InputContainer = styled.div<InputContainerProps>`
+  position: relative;
+  border-radius: 15px;
+  border: 2px solid
+    ${(p) =>
+      p.error
+        ? colors.failedValite
+        : p.hasValue
+        ? colors.successValidate
+        : colors.primaryColor};
+
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+export const StyledEye = styled(Eye)`
+  position: absolute;
+  right: 10px;
+  cursor: pointer;
+`;
+export const StyledHiddenEye = styled(HidedEye)`
+  position: absolute;
+  right: 10px;
+  cursor: pointer;
+`;
+export const StyledInput = styled.input<InputProps>`
+  padding: 13px 25px;
+  border: none;
+`;
+export const Error = styled.p`
+  color: red;
+`;
+export const Label = styled.h3`
+  color: ${colors.primaryColor};
+  font-weight: 600;
+  margin: 5px 0;
+`;
