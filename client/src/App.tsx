9655f3f52a231eb82bb789d7bf66e22ae700573d
Ömer Şahin <63051255+1sahinomer1@users.noreply.github.com>
2021-12-06 17:27:12 +0300

    remove navbar and add privateroute

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { Navbar } from './components';
+import { PrivateRoute } from './components';
 
 import { ROUTES } from './constants/routes';
 
@@ -12,10 +12,9 @@ const Register = lazy(() => import('./pages/Register'));
 const App = () => {
   return (
     <Router>
-      <Navbar />
       <Suspense fallback={<h2>loading</h2>}>
         <Switch>
-          <Route exact path={ROUTES.HOME} component={Home} />
+          <PrivateRoute exact path={ROUTES.HOME} component={Home} />
           <Route exact path={ROUTES.LOGIN} component={Login} />
           <Route exact path={ROUTES.REGISTER} component={Register} />
         </Switch>
